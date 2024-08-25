ef91c1232623cf3d61c09b57de150f786b5531ff
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2024-08-25 11:58:20 +0900

    LKMのテンプレートを作成した

---
+++ playground/lkms/01-template/template-lkm.c
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-2.0
+#define pr_fmt(fmt) "%s:%s(): " fmt, KBUILD_MODNAME, __func__
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+#define OURMODNAME   "lkm_template"
+
+MODULE_AUTHOR("Kaiwan N Billimoria");
+MODULE_DESCRIPTION("a simple LKM template; do refer to the (better) Makefile as well");
+MODULE_LICENSE("Dual MIT/GPL");	// or whatever
+MODULE_VERSION("0.1");
+
+static int __init lkm_template_init(void)
+{
+	pr_info("inserted\n");
+	return 0;		/* success */
+}
+
+static void __exit lkm_template_exit(void)
+{
+	pr_info("removed\n");
+}
+
+module_init(lkm_template_init);
+module_exit(lkm_template_exit);
